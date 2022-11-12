ba452aeb6235dc7bb511151fb32f4e2af6d1a6f6
Arne Goeteyn <arne.goeteyn@officient.io>
2022-11-12 19:25:40 +0100

    switch to formulo

--- src/Server/Recipes.hs
+++ src/Server/Recipes.hs
@@ -18,59 +18,59 @@ import Servant.API.Generic
 import Servant.Server.Generic (AsServer)
 
 data RecipeRoutes mode = RecipeRoutes
-  { list :: mode :- "recipes" :> Get '[JSON] [Entity Recipe],
-    find ::
-      mode :- "recipes" :> Capture "recipeId" RecipeId
-        :> Get '[JSON] (Entity Recipe, [Entity Ingredient]),
-    create ::
-      mode :- "recipes"
-        :> ReqBody '[JSON] Recipe
-        :> Post '[JSON] Int64,
-    putRecipeIngredients ::
-      mode :- "recipes" :> Capture "recipeId" RecipeId
-        :> "ingredients"
-        :> ReqBody '[JSON] [PutRecipeIngredient]
-        :> Put '[JSON] [RecipeIngredientsId]
-  }
-  deriving (Generic)
+    { list :: mode :- "recipes" :> Get '[JSON] [Entity Recipe]
+    , find ::
+        mode :- "recipes" :> Capture "recipeId" RecipeId
+            :> Get '[JSON] (Entity Recipe, [Entity Ingredient])
+    , create ::
+        mode :- "recipes"
+            :> ReqBody '[JSON] Recipe
+            :> Post '[JSON] Int64
+    , putRecipeIngredients ::
+        mode :- "recipes" :> Capture "recipeId" RecipeId
+            :> "ingredients"
+            :> ReqBody '[JSON] [PutRecipeIngredient]
+            :> Put '[JSON] [RecipeIngredientsId]
+    }
+    deriving (Generic)
 
 recipesServer :: PGInfo -> RecipeRoutes AsServer
 recipesServer ctx =
-  RecipeRoutes
-    { list = getRecipesHandler ctx,
-      find = getRecipeHandler ctx,
-      create = createRecipeHandler ctx,
-      putRecipeIngredients = putRecipeHandler ctx
-    }
+    RecipeRoutes
+        { list = getRecipesHandler ctx
+        , find = getRecipeHandler ctx
+        , create = createRecipeHandler ctx
+        , putRecipeIngredients = putRecipeHandler ctx
+        }
 
 getRecipesHandler :: PGInfo -> Handler [Entity Recipe]
 getRecipesHandler conn = liftIO $ selectRecipes conn
 
 getRecipeHandler ::
-  PGInfo -> RecipeId -> Handler (Entity Recipe, [Entity Ingredient])
+    PGInfo -> RecipeId -> Handler (Entity Recipe, [Entity Ingredient])
 getRecipeHandler recipeId conn = do
-  maybeRecipe <- liftIO $ selectRecipe recipeId conn
-  case maybeRecipe of
-    Just recipe -> return recipe
-    Nothing ->
-      Handler
-        (throwError $ err404 {errBody = "A recipe with that id can't be found"})
+    maybeRecipe <- liftIO $ selectRecipe recipeId conn
+    case maybeRecipe of
+        Just recipe -> return recipe
+        Nothing ->
+            Handler
+                (throwError $ err404{errBody = "A recipe with that id can't be found"})
 
 createRecipeHandler :: PGInfo -> Recipe -> Handler Int64
 createRecipeHandler conn recipe = do
-  duplicate <- liftIO $ findRecipeWithTitle conn $ recipeTitle recipe
-  case duplicate of
-    Nothing -> liftIO $ createRecipe conn recipe
-    Just _ ->
-      Handler
-        ( throwError $
-            err401 {errBody = "A recipe with that name already exists"}
-        )
+    duplicate <- liftIO $ findRecipeWithTitle conn $ recipeTitle recipe
+    case duplicate of
+        Nothing -> liftIO $ createRecipe conn recipe
+        Just _ ->
+            Handler
+                ( throwError $
+                    err401{errBody = "A recipe with that name already exists"}
+                )
 
 putRecipeHandler ::
-  PGInfo ->
-  RecipeId ->
-  [PutRecipeIngredient] ->
-  Handler [RecipeIngredientsId]
+    PGInfo ->
+    RecipeId ->
+    [PutRecipeIngredient] ->
+    Handler [RecipeIngredientsId]
 putRecipeHandler conn recipe ingredients =
-  liftIO $ setRecipeIngredients conn recipe ingredients
+    liftIO $ setRecipeIngredients conn recipe ingredients
