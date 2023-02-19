0a199e7c7d743eb4a126cca83d5c15dabe462512
Arne Goeteyn <arne.goeteyn@officient.io>
2023-02-19 14:29:21 +0100

    add readme and format something with stylish-haskell

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # kurkuma
+
+Small project to save recipes. Mostly just some quick toying around with Servant to see what that's all about.
+
--- src/Server/Recipes.hs
+++ src/Server/Recipes.hs
@@ -1,83 +1,92 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Server.Recipes where
 
-import Control.Monad.Cont
-import Control.Monad.Error.Class
-import Data.Int (Int64)
-import Database (PGInfo)
-import Database.Persist hiding (delete)
-import Repositories.Recipe
-import Schema
-import Servant
-import Servant.API.Generic
-import Servant.Server.Generic (AsServer)
+import           Control.Monad.Cont
+import           Control.Monad.Error.Class
+import           Data.Int                  (Int64)
+import           Database                  (PGInfo)
+import           Database.Persist          hiding (delete)
+import           Repositories.Recipe
+import           Schema
+import           Servant
+import           Servant.API.Generic
+import           Servant.Server.Generic    (AsServer)
 
 data RecipeRoutes mode = RecipeRoutes
-    { list :: mode :- "recipes" :> Get '[JSON] [Entity Recipe]
-    , find ::
-        mode :- "recipes" :> Capture "recipeId" RecipeId
-            :> Get '[JSON] (Entity Recipe, [Entity Ingredient])
-    , create ::
-        mode :- "recipes"
-            :> ReqBody '[JSON] Recipe
-            :> Post '[JSON] Int64
-    , delete ::
-        mode :- "recipes"
-            :> Capture "recipeId" RecipeId
-            :> Delete '[JSON] ()
-    , putRecipeIngredients ::
-        mode :- "recipes" :> Capture "recipeId" RecipeId
-            :> "ingredients"
-            :> ReqBody '[JSON] [PutRecipeIngredient]
-            :> Put '[JSON] [RecipeIngredientsId]
-    }
-    deriving (Generic)
+  { list :: mode :- "recipes" :> Get '[JSON] [Entity Recipe],
+    find ::
+      mode
+        :- "recipes"
+        :> Capture "recipeId" RecipeId
+        :> Get '[JSON] (Entity Recipe, [Entity Ingredient]),
+    create ::
+      mode
+        :- "recipes"
+        :> ReqBody '[JSON] Recipe
+        :> Post '[JSON] Int64,
+    delete ::
+      mode
+        :- "recipes"
+        :> Capture "recipeId" RecipeId
+        :> Delete '[JSON] (),
+    putRecipeIngredients ::
+      mode
+        :- "recipes"
+        :> Capture "recipeId" RecipeId
+        :> "ingredients"
+        :> ReqBody '[JSON] [PutRecipeIngredient]
+        :> Put '[JSON] [RecipeIngredientsId]
+  }
+  deriving (Generic)
 
 recipesServer :: PGInfo -> RecipeRoutes AsServer
 recipesServer ctx =
-    RecipeRoutes
-        { list = getRecipesHandler ctx
-        , find = getRecipeHandler ctx
-        , create = createRecipeHandler ctx
-        , delete = deleteRecipeHandler ctx
-        , putRecipeIngredients = putRecipeHandler ctx
-        }
+  RecipeRoutes
+    { list = getRecipesHandler ctx,
+      find = getRecipeHandler ctx,
+      create = createRecipeHandler ctx,
+      delete = deleteRecipeHandler ctx,
+      putRecipeIngredients = putRecipeHandler ctx
+    }
+
+-- Handlers
 
 getRecipesHandler :: PGInfo -> Handler [Entity Recipe]
 getRecipesHandler conn = liftIO $ selectRecipes conn
 
 getRecipeHandler ::
-    PGInfo -> RecipeId -> Handler (Entity Recipe, [Entity Ingredient])
+  PGInfo -> RecipeId -> Handler (Entity Recipe, [Entity Ingredient])
 getRecipeHandler recipeId conn = do
-    maybeRecipe <- liftIO $ selectRecipe recipeId conn
-    case maybeRecipe of
-        Just recipe -> return recipe
-        Nothing ->
-            Handler
-                (throwError $ err404{errBody = "A recipe with that id can't be found"})
+  maybeRecipe <- liftIO $ selectRecipe recipeId conn
+  case maybeRecipe of
+    Just recipe -> return recipe
+    Nothing ->
+      Handler
+        (throwError $ err404 {errBody = "A recipe with that id can't be found"})
 
 createRecipeHandler :: PGInfo -> Recipe -> Handler Int64
 createRecipeHandler conn recipe = do
-    duplicate <- liftIO $ findRecipeWithTitle conn $ recipeTitle recipe
-    case duplicate of
-        Nothing -> liftIO $ createRecipe conn recipe
-        Just _ ->
-            Handler
-                ( throwError $
-                    err401{errBody = "A recipe with that name already exists"}
-                )
+  duplicate <- liftIO $ findRecipeWithTitle conn $ recipeTitle recipe
+  case duplicate of
+    Nothing -> liftIO $ createRecipe conn recipe
+    Just _ ->
+      Handler
+        ( throwError $
+            err401 {errBody = "A recipe with that name already exists"}
+        )
+
 deleteRecipeHandler :: PGInfo -> RecipeId -> Handler ()
 deleteRecipeHandler conn i = liftIO $ deleteRecipe conn i
 
 putRecipeHandler ::
-    PGInfo ->
-    RecipeId ->
-    [PutRecipeIngredient] ->
-    Handler [RecipeIngredientsId]
+  PGInfo ->
+  RecipeId ->
+  [PutRecipeIngredient] ->
+  Handler [RecipeIngredientsId]
 putRecipeHandler conn recipe ingredients =
-    liftIO $ setRecipeIngredients conn recipe ingredients
+  liftIO $ setRecipeIngredients conn recipe ingredients
